949dd7f8c211ff7562ae25694afc9c20f97e5e95
Roi Snir <roisnr132@gmail.com>
2021-10-09 17:32:37 +0300

    added tmLanguage inheritance

---
+++ bpf/prepare_syntaxes.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const bpfLang = JSON.parse(fs.readFileSync('./syntaxes/bpf.tmLanguage.json'));
+const bbpfLang = JSON.parse(fs.readFileSync('./syntaxes/bbpf.tmLanguage.json.in'));
+bbpfLang.patterns = [...bpfLang.patterns, ...bbpfLang.patterns];
+bbpfLang.repository = {...bpfLang.repository, ...bbpfLang.repository};
+fs.writeFileSync("./syntaxes/bbpf.tmLanguage.json", JSON.stringify(bbpfLang, null, 2));
+console.log("Generated ./syntaxes/bbpf.tmLanguage.json Successfully!");
